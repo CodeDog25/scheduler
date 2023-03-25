2d88e6ebd1d9346d5524a91d5678fdb27f52670f
CodeDog25 <xishading@Shiyus-iMac-2.local>
2023-03-25 17:13:57 -0600

    change code to use onChange and value

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,21 +2,17 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
-  const { days, day, setDay } = props;
-
-  const dayListItems = days.map((dayObj) => (
-    <DayListItem
-      key={dayObj.id}
-      name={dayObj.name}
-      spots={dayObj.spots}
-      selected={dayObj.name === day}
-      setDay={() => setDay(dayObj.name)}
-    />
-  ));
-  
   return (
     <ul>
-      {dayListItems}
+      {props.days.map((day) => (
+        <DayListItem
+          key={day.id}
+          name={day.name}
+          spots={day.spots}
+          selected={day.name === props.value}
+          setDay={props.onChange}
+        />
+      ))}
     </ul>
   );
 }
