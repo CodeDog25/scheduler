b86e9b158224e34d5f2ffc552475b161cbc17013
CodeDog25 <xishading@Shiyus-iMac-2.local>
2023-04-20 09:24:33 -0600

    add debugging test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,7 +9,8 @@ import {
   getAllByTestId,
   getByAltText,
   getByPlaceholderText,
-  prettyDOM
+  prettyDOM,
+  queryByText
  } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -27,7 +28,7 @@ describe("Application", () => {
 });
 
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -39,10 +40,18 @@ describe("Application", () => {
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
-    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
 
-    console.log(prettyDOM(appointment));
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+    console.log(prettyDOM(day));
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 });
